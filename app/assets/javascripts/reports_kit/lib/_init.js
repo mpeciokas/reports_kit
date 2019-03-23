17cd4210cb5f75e626495920810791b0753431b1
mpeciokas <37046749+mpeciokas@users.noreply.github.com>
2019-03-23 09:25:21 +0100

    Update _init.js

--- app/assets/javascripts/reports_kit/lib/_init.js
+++ app/assets/javascripts/reports_kit/lib/_init.js
@@ -1,9 +1,13 @@
 window.ReportsKit = {};
 
-$(document).ready(function() {
+//$(document).ready(function() {
+var ready = function() {
   $('.reports_kit_report').each(function(index, el) {
     var el = $(el)
     var reportClass = el.data('report-class');
     new ReportsKit[reportClass]().render({ 'el': el });
   });
-});
+ }
+//});
+
+$(document).on('turbolinks:load', ready);
